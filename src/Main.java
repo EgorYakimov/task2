8e398d817cbd140c9859444de53c2a586561d1dc
EgorYakimov <151763291+EgorYakimov@users.noreply.github.com>
2024-01-09 10:28:56 +0500

    changed Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,16 +5,16 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-    int openingBalance = 100;
-    int replenishment = 700; //здесь можно менять значение
-    int sumTotal = 0;
-    int bonus = 0;
-    if (replenishment > 1000){
-        sumTotal = ((openingBalance + replenishment)/100+(openingBalance + replenishment));
-        bonus = (openingBalance + replenishment)/100;
-    }
-    System.out.printf("Баланс Вашего счёта " + (openingBalance + replenishment + bonus) + " руб.");
-    System.out.println("");
-    System.out.printf("Ваш бонус " + bonus + " руб.");
+        int openingBalance = 100;
+        int replenishment = 700; //здесь можно менять значение
+        int sumTotal = 0;
+        int bonus = 0;
+        if (replenishment > 1000) {
+            sumTotal = ((openingBalance + replenishment) / 100 + (openingBalance + replenishment));
+            bonus = (openingBalance + replenishment) / 100;
+        }
+        System.out.printf("Баланс Вашего счёта " + (openingBalance + replenishment + bonus) + " руб.");
+        System.out.println("");
+        System.out.printf("Ваш бонус " + bonus + " руб.");
     }
 }
